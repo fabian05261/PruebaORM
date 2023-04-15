8b26d05e77a0fc860808c0d75f652216cb70f3a3
fabian05261 <fabianzapata2001@hotmail.com>
2023-04-14 20:47:35 -0500

    Add files via upload

---
+++ App/BasedeDatos/Datos.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace App
+{
+  public class DatabaseContext : DbContext
+  {
+    public DbSet<Product> Products { get; set; }
+
+    public DbSet<OrderItem> OrderItems { get; set; }
+
+    public DbSet<Order> Orders { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseSqlite("Data Source=database.db");
+    }
+  }
+
+  public class Order
+  {
+    public int OrderId { get; set; }
+
+    public DateTime? Created { get; set; }
+  
+    public ICollection<OrderItem> Items { get; set; }
+  }
+
+  public class OrderItem
+  {
+    public int OrderItemId { get; set; }
+    public int Quantity { get; set; }
+    public virtual Product Product { get; set; }
+  }
+
+  public class Product 
+  {
+    public int ProductId { get; set; }
+    public double Price { get; set; }
+    public string Description { get; set; }
+  }
+}
---
+++ App/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new DatabaseContext())
+            {
+                var product = new Product(){ Price = 100, Description = "Movie" };
+                if(product != null)
+                {
+                    var item = new OrderItem
+                    {
+                        Quantity = 1,
+                        Product = product
+                    };
+                    db.OrderItems.Add(item);
+                    db.SaveChanges();
+                    Console.WriteLine("{0} {1} Product: {2}", item.OrderItemId, item.Quantity, item.Product.Description);
+                }
+            }
+
+        }
+        private static void crearProducto()
+        {
+            using (var db = new DatabaseContext())
+            {
+    
+                var product = new Product(){ Price = 100, Description = "Movie" };
+                db.Products.Add(product);
+                db.SaveChanges();
+
+                foreach(var p in db.Products) 
+                {
+                    Console.WriteLine("{0} {1} {2}", p.ProductId, p.Description, p.Price);
+                }            
+            }
+        }
+        private static void actualizarProducto()
+        {
+            using(var db = new DatabaseContext()) 
+            {
+                var item = db.OrderItems.SingleOrDefault();
+                item.Quantity++;
+                db.SaveChanges();
+            }
+        }
+        private static void eliminarProducto()
+        {
+            using(var db = new DatabaseContext()) 
+            {
+                var product = db.Products.SingleOrDefault();
+                db.Products.Remove(product);
+                db.SaveChanges();
+            }
+        }
+    }
+}
+
